477b00e6e049aeb97e2f136cae3f20fa68d4a1a3
RhuanCSM <rcsm.martin@gmail.com>
2024-05-27 14:25:47 -0300

    Add username and email validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :blog_posts
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 }
 end
